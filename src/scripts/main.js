0b0d781f4266d879a7475b220fb4bdbf031b8b3d
SamBritt <sam.britt2@yahoo.com>
2019-03-07 09:52:51 -0600

    added flight handler logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,44 @@
-//Mirror, Mirror in the code
+//Mirror, Mirror in the code**
 
-let inputField = document.querySelector('#message');
-let section1 = document.getElementById('one');
-let section2 = document.getElementById('two');
+// let inputField = document.querySelector('#message');
+// let section1 = document.getElementById('one');
+// let section2 = document.getElementById('two');
 
-inputField.addEventListener('keyup', (e)=>{
-    section1.textContent= e.target.value;
-    section2.textContent = e.target.value;
-})
+// inputField.addEventListener('keyup', (e) => {
+//     section1.textContent = e.target.value;
+//     section2.textContent = e.target.value;
+// })
 
-//Scrolling and Scaling
+//Scrolling and Scaling**
 
-const audrey = document.getElementById("audrey");
+// const audrey = document.getElementById("audrey");
 
-audrey.addEventListener("scroll", () =>{
-   audrey.style.width = window.scrollY / 3;
-   audrey.width = window.scrollY / 4;
-   console.log(event.target);
-})
+// document.addEventListener('scroll', (e) => {
 
-    /*
-        Adjust the width of audrey to be 1/3 the value of
-        `window.scrollY`. No lower than 50px, though.
-    */
+//     let windowScrollY = window.scrollY / 3;
+//     let windowScrollY2 = window.scrollY / 4
 
 
+//     audrey.style.width = `${windowScrollY}px`;
+//     audrey.style.minWidth = '50px';
+//     audrey.style.height = `${windowScrollY2}px`;
+//     audrey.style.minHeight = '100px';
+// })
 
-    /*
-        Adjust the height of audrey to be 1/4 the value of
-        `window.scrollY`. No lower than 100px, though.
-    */
+//Window Powers Activate!**
+
+let activateFlight = document.getElementById('flight');
+
+document.querySelector("#activate-flight").addEventListener('click', (e) => {
+    flightHandlerFunction();
+});
+
+function flightHandlerFunction(){
+    if(activateFlight.className == 'power disabled'){
+        activateFlight.classList.toggle('enabled');
+    }else if(activateFlight.className == 'power enabled'){
+        activateFlight.classList.toggle('disabled');
+    }
+}
+
+
